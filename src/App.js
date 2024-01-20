890ff460cdc2864a0132a69b8ae5c22f6dfb4da4
Alejandro <alejo20788@hotmail.com>
2024-01-20 12:47:02 -0500

    Feat:[no story][Alejandro] I make the project header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center mt-5">
+      <h1 className="display-4">Central de Emergencia</h1>
+      <div className="mt-4 d-flex flex-column align-items-center">
+        <div className="d-flex align-items-center mb-2">
+          <label className="mr-2">Emergencia</label>
+          <input
+            type="text"
+            className="form-control form-control-sm mr-2"
+            placeholder="Ingrese texto..."
+          />
+          <button className="btn btn-primary" type="button">Ingresar</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
