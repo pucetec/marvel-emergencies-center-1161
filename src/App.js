28810d981c266505c8f336fbb80a9a87d7595a63
Josue Auquilla <josueesteban0724@gmail.com>
2024-01-20 12:45:09 -0500

    Feat:[No Story][Josue]Create a principal page to control emergency with material-UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ textAlign: "center" }}>Central de Emergencias</h1>
+      <br />
+      <div style={{ textAlign: "center" }}>
+        <label>
+          Emergencia <input type="text" value="" />
+        </label>
+        <Button variant="contained">Ingresar</Button>
+      </div>
+      <br />
+      <div>
+        <h2 style={{ textAlign: "center" }}>Emergencias sin signar</h2>
+        <table style={{ margin: "auto", textAlign: "center" }}>
+          <td>#</td>
+          <td>Emergencias</td>
+          <td>Acciones </td>
+          <Button variant="contained" startIcon={<DeleteIcon />}></Button>
+        </table>
+      </div>
+      <br />
+      <div>
+        <h2 style={{ textAlign: "center" }}>Emergencias Asignadas</h2>
+        <table style={{ margin: "auto", textAlign: "center" }}>
+          <td>#</td>
+          <td>Emergencias</td>
+          <td>Héroe</td>
+          <td>Acciones </td>
+          <Button variant="contained" startIcon={<DeleteIcon />}></Button>
+        </table>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
