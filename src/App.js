d04e07f120bbaf4d9637d4f23efc55552f430100
Fernando <gm8963383@gmail.com>
2024-01-20 12:51:50 -0500

    NOSTORY: [CDE] [Fernando] Add components title, button and table for emergency central

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [emergencias, setEmergencias] = useState([]);
+
+  const agregarEmergencia = () => {
+    if (inputValue.trim() !== "") {
+      setEmergencias([...emergencias, inputValue]);
+      setInputValue("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="centrado">
+      <h1>Tu Título</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={agregarEmergencia}>Agregar Emergencia</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Emergencias</th>
+          </tr>
+        </thead>
+        <tbody>
+          {emergencias.map((emergencia, index) => (
+            <tr key={index}>
+              <td>{emergencia}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
