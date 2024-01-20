9b6a417f710f5755bc3987e3b0e3aeb2c5a861e2
Jose Ignacio Alvarez Torres Naranjo <nacho.alvarezt@live.com>
2024-01-20 12:40:55 -0500

    feat:[NoStory][Jose Alvarez Torres] started working on layout of page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Box, TextField, Button } from "@mui/material";
+import { left } from "@popperjs/core";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box>
+        <h1>Central de Emergencias</h1>
+        <Box sx={{ display: "flex", width: "80%", margin: "auto", justifyContent: "center" }}>
+          <TextField label="Emergencia" variant="outlined" sx={{ mx: "20px" }} />
+          <Button variant="contained"> Submit </Button>
+        </Box>
+      </Box>
     </div>
   );
 }
