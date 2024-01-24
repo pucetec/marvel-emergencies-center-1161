3906003b1efc72eafa887b22f15a31bf02b12d0d
Fernando <gm8963383@gmail.com>
2024-01-23 20:25:42 -0500

    NOSTORY: [MEC][FERNANDO] Add new components for the emergencies center

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
+import EmergencyCenterPage from "./EmergencyCenterPage";
 
 const App = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [emergencias, setEmergencias] = useState([]);
-
-  const agregarEmergencia = () => {
-    if (inputValue.trim() !== "") {
-      setEmergencias([...emergencias, inputValue]);
-      setInputValue("");
-    }
-  };
-
   return (
-    <div className="centrado">
-      <h1>Tu Título</h1>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <button onClick={agregarEmergencia}>Agregar Emergencia</button>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Emergencias</th>
-          </tr>
-        </thead>
-        <tbody>
-          {emergencias.map((emergencia, index) => (
-            <tr key={index}>
-              <td>{emergencia}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div classname="App">
+      <EmergencyCenterPage />
     </div>
   );
 };
