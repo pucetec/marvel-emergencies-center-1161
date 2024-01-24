2a08d6c0790925cd0d0e77c6ab3493e1d67c9edb
Jose Ignacio Alvarez Torres Naranjo <nacho.alvarezt@live.com>
2024-01-23 20:28:18 -0500

    feat:[NoStory][Jose Alvarez Torres] added fetch to marvel api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import * as React from "react";
+import react, { useState, useEffect } from "react";
 import { Box, TextField, Button } from "@mui/material";
-import { left } from "@popperjs/core";
+import getCharacters from "./Misc/getMarvelCharacter";
 
-function App() {
+const App = () => {
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      const response = await getCharacters();
+      setCharacters(response);
+    };
+    if (characters.length === 0) {
+      fetchCharacters();
+    }
+  }, []);
+  useEffect(() => {
+    console.log(characters);
+  }, [characters]);
   return (
     <div className="App">
       <Box>
-        <h1>Central de Emergencias</h1>
+        <h1 style={{ textAlign: "center" }}>Central de Emergencias</h1>
         <Box sx={{ display: "flex", width: "80%", margin: "auto", justifyContent: "center" }}>
           <TextField label="Emergencia" variant="outlined" sx={{ mx: "20px" }} />
           <Button variant="contained"> Submit </Button>
         </Box>
       </Box>
+      <Box>
+        <h2 style={{ textAlign: "center" }}>Central de Emergencias</h2>
+        <Box>
+          {characters.length > 0 &&
+            characters.map((item, i) => {
+              return <p>{item.name}</p>;
+            })}
+        </Box>
+      </Box>
     </div>
   );
-}
+};
 
 export default App;
