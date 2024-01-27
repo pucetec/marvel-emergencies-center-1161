2ac5082044d0f7c888b7749ba568ec3cce765ad5
ismael freire <jifreirez@puce.edu.ec>
2024-01-27 12:44:57 -0500

    feat[nostory][ismael freire]creacion de ventana  e implementacion del api marvel

--- src/componets/modal/Modal.js
+++ src/componets/modal/Modal.js
@@ -1,17 +1,21 @@
+// Modal.js
 import React from 'react';
+import './Modal.css';
 
 const Modal = ({ visible, onClose }) => {
   return (
     visible && (
       <div className="modal-overlay">
         <div className="modal">
-          <div className="modal-content">
-            <h2>Agregar a tu super</h2>
-            {/* Contenido del modal */}
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+          <div className="modal-header">
+            <h2>Agrega a tu super</h2>
             <button className="close-button" onClick={onClose}>
               X
             </button>
+          </div>
+          <div className="modal-content">
+            {/* Contenido del modal */}
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
           </div>
         </div>
       </div>
@@ -20,6 +24,7 @@ const Modal = ({ visible, onClose }) => {
 };
 
 export default Modal;
+
 
 
 
