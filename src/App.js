2a17622b4ed0eb5e5bb89c3632e318bb8f06b2c8
ismael freire <jifreirez@puce.edu.ec>
2024-01-30 20:11:43 -0500

    feat[nostory][Ismael Freire] creacion del modulo con la informacion de marvel y su tabla de asignacion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 // App.js
 import React, { useState } from 'react';
-import EntradaEmergencia from './componets/EntradaEmergencias/EntradaEmergencias';
 import ListaEmergencias from './componets/ListaEmergencias/ListaEmergencias';
-import Modal from './componets/modal/Modal'; // Asegúrate de importar el componente Modal
+import Modal from './componets/modal/Modal';
+import ListaEmergenciaAsignada from './componets/ListaEmergenciaAsignada/ListaEmergenciaAsignada'; // Importa el nuevo componente
+import EntradaEmergencia from './componets/EntradaEmergencias/EntradaEmergencias';
 
 const App = () => {
   const [emergenciasList, setEmergenciasList] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
+  const [emergencyAssignments, setEmergencyAssignments] = useState([]);
 
   const agregarEmergencia = (nuevaEmergencia) => {
     setEmergenciasList([...emergenciasList, nuevaEmergencia]);
   };
 
-  const handleMasClick = () => {
+  const handleMasClick1 = () => {
     setModalVisible(true);
   };
 
-  const handleBasuraClick = (index) => {
+  const handleBasuraClick1 = (index) => {
     const nuevasEmergenciasList = [...emergenciasList];
     nuevasEmergenciasList.splice(index, 1);
     setEmergenciasList(nuevasEmergenciasList);
+  };
+
+  const handleAsignarHeroe = (superheroName) => {
+    // Agregar lógica para obtener la emergencia seleccionada aquí
+    const selectedEmergency = emergenciasList[0]; // Por ahora, seleccionamos la primera emergencia de la lista
+
+    const hero = superheroName;
+    const newAssignment = { emergency: selectedEmergency, hero }; // Pasar la emergencia seleccionada como parte de la asignación
+    setEmergencyAssignments([...emergencyAssignments, newAssignment]);
+    setModalVisible(false);
   };
 
   const closeModal = () => {
@@ -33,16 +45,17 @@ const App = () => {
       {emergenciasList.length > 0 && (
         <ListaEmergencias
           emergenciasList={emergenciasList}
-          onMasClick={handleMasClick}
-          onBasuraClick={handleBasuraClick}
+          onMasClick={handleMasClick1}
+          onBasuraClick={handleBasuraClick1}
         />
       )}
 
-      <Modal visible={modalVisible} onClose={closeModal} />
+      <Modal visible={modalVisible} onClose={closeModal} onAsignarHeroe={handleAsignarHeroe} />
+
+      {/* Nueva tabla para mostrar emergencias asignadas */}
+      <ListaEmergenciaAsignada assignments={emergencyAssignments} />
     </div>
   );
 }
 
 export default App;
-
-
--- src/componets/Marvel/Marvel.js
+++ src/componets/Marvel/Marvel.js
@@ -1,16 +1,16 @@
 // marvelAPI.js
-import md5 from 'md5'; // Asegúrate de instalar esta dependencia antes de usarla
+import md5 from 'md5';
 
 const PUBLIC_KEY = '79f4e56ee9988ac936410ebb288047db';
 const PRIVATE_KEY = '612b7cef627a605a89525dc5cc0eef7c070f7768';
-const API_BASE_URL = 'developer.marvel.com';
+const API_BASE_URL = 'https://gateway.marvel.com';
 
-const getMarvelData = async () => {
+const GetMarvelData = async () => {
   const timestamp = new Date().getTime();
   const hash = md5(`${timestamp}${PRIVATE_KEY}${PUBLIC_KEY}`);
-  const response = await fetch(`${API_BASE_URL}/characters?ts=${timestamp}&apikey=${PUBLIC_KEY}&hash=${hash}`);
+  const response = await fetch(`${API_BASE_URL}/v1/public/characters?ts=${timestamp}&apikey=${PUBLIC_KEY}&hash=${hash}`);
   const data = await response.json();
   return data;
 };
 
-export default getMarvelData;
+export default GetMarvelData;
