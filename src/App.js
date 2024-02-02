93e7cc6519decf05a9c252faca8daee6a3d9fe3a
Fabián Aguirre <fabianraf@gmail.com>
2024-02-02 12:43:52 -0500

    Revert "cretion componet texttitle,table,textinput"

--- src/App.js
+++ src/App.js
@@ -1,54 +1,25 @@
-import TextInput from './common/textinput/text';
-import TextTitle from './common/texttitle/title';
-import TableMarvel from './common/table/table';
-import InsertButton from './common/button/button';
-import { DeleteIcon,CheckIcon } from '@chakra-ui/icons';
-import React,{useState} from'react';
+import logo from './logo.svg';
+import './App.css';
 
-const Public_keY="640d1a3f181daef1d461ca9e45431977";
-const Private_key="6742d62f048eea0e5bba41006af05a1e7c8b4af6";
-const Gateway="http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150";
-
-const App=()=>{
-  const [emergencies,setEmergencies]=useState([]);
-  const [inputvalue,setInputValue]=useState('');
-  
-  const handleInputChange=(event)=>{
-    setInputValue(event.target.value);
-  }
-  const handleAddEmergency=()=>{
-    if(inputvalue.trim() !==''){
-      const newEmergency = {
-        id:emergencies.length+1,
-        name:inputvalue.trim(),
-      };
-      setEmergencies([...emergencies,newEmergency]);
-      setInputValue('');
-    }
-  };
-  return(
-    <div align='center'>
-      <TextTitle/>
-      Emergencias
-      <TextInput value={inputvalue} onChange={handleInputChange}/>
-      <InsertButton onClick={handleAddEmergency}/>
-      <br></br>
-      <strong>Emergencias sin Asignar</strong> 
-      <br></br>
-      <TableMarvel
-      headers={["#","emergencias","Acciones"]}
-      bodyRows={emergencies.map((emergencies)=>[emergencies.id,emergencies.name,])}>
-        </TableMarvel>
-        <br></br>
-      <strong>Emergencias Asignadas</strong>
-      <TableMarvel
-      headers={[" #"," Emergencias "," Heroe "," Acciones "]}
-      bodyRows={["nuevo","dato","thor",<div><DeleteIcon/><CheckIcon/></div>]}>
-      </TableMarvel>
-      
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
-
 
 export default App;
--- src/common/button/button.js
+++
@@ -1,8 +0,0 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
-
-const InsertButton =(onClick)=>{
-    return (
-        <Button colorScheme='blue' size='sm' onClick={(onClick)}>Insertar </Button>
-    );
-};
-export default InsertButton;
